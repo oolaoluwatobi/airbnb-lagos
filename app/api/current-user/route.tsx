c09ced1c8f42db10513145674c2e0ff6a6479229
oolaoluwatobi <58662625+oolaoluwatobi@users.noreply.github.com>
2024-03-13 22:09:27 +0100

    Add OPTIONS method to current-user route

--- app/api/current-user/route.tsx
+++ app/api/current-user/route.tsx
@@ -5,6 +5,10 @@ import getCurrentUser from "@/app/actions/getCurrentUser";
 import { getServerSession } from "next-auth";
 import getUser from "@/app/actions/getUser";
 
+interface IParams {
+  email?: string;
+}
+
 export async function GET(request: Request) {
   console.log("GETTING...");
 
@@ -12,6 +16,36 @@ export async function GET(request: Request) {
     // const currentUser = await getCurrentUser();
     const session = await getServerSession();
     const currentUser = await getUser({ userEmail: session?.user?.email });
+
+    if (!currentUser) {
+      return NextResponse.json({
+        message: "Unauthorized",
+        status: 401,
+        data: null,
+      });
+    }
+
+    return NextResponse.json({
+      message: "Success",
+      status: 200,
+      data: currentUser,
+    });
+  } catch (error: any) {
+    console.log("ERROR :", error, error?.message);
+    return NextResponse.json({
+      Error: "Something went wrong, failed to get user",
+    });
+  }
+}
+
+export async function OPTIONS(request: Request) {
+  console.log("OPTIONS...");
+  const { data } = await request.json();
+  const email = data;
+  try {
+    // const currentUser = await getCurrentUser();
+    const session = await getServerSession();
+    const currentUser = await getUser({ userEmail: data });
 
     if (!currentUser) {
       return NextResponse.json({
